78c56076b663f1be8d5037656ab76faab7fdf07f
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2024-12-23 14:00:27 +0300

    feat: added auth via github

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,5 +2,5 @@ import Image from 'next/image';
 
 export default function Home() {
   console.log('What is happening?');
-  return <h1>Hello Next.js</h1>;
+  return <h1 className='text-3xl'>Hello Next.js</h1>;
 }
